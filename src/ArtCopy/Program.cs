8597630b0ecea10cf781625b181dd24863dee7c6
Sergey Azarkevich <azarkevich@gmail.com>
2017-10-03 23:39:14 +0300

    Project created

---
+++ src/ArtCopy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArtCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
